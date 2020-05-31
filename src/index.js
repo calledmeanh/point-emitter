cbf91d8a455b3274e62c2c1dbb369925c22a91f7
calledmeanh <calledmeanh@gmail.com>
2020-06-01 01:07:48 +0700

    beta 0.0.2

--- src/index.js
+++ src/index.js
@@ -16,65 +16,81 @@ class DragWithStyle {
     return childrens;
   }
 
-  addMouseEvent() {
-    const childrens = this.getChildrens();
-    let originTop = 0;
-    let originLeft = 0;
-    childrens.forEach((child, i) => {
-      child.addEventListener("mousedown", function (e) {
-        child.classList.add("touched");
-        document.addEventListener("mousemove", mousemove);
-        document.addEventListener("mouseup", mouseup);
-        document.addEventListener("mouseover", mouseover);
-        document.addEventListener("mouseleave", mouseleave);
+  addAttr(el, id) {
+    if (!el.hasAttribute("draggable")) {
+      el.setAttribute("draggable", true);
+      el.setAttribute("data-id", id);
+    }
+    return el;
+  }
+
+  makeChildrenDraggable(elements) {
+    const draggableEl = elements.map((el, index) => this.addAttr(el, index));
+    return draggableEl;
+  }
+
+  onChildrenDragging(draggableEls) {
+    draggableEls.forEach((el) => {
+      el.addEventListener("dragstart", function (e) {
+        el.classList.add("dragging");
       });
-
-      function mousemove(e) {
-        const { width, height } = child.getBoundingClientRect();
-        /* cach 1 */
-        const centerXForTranslate = e.pageX - child.offsetLeft - width / 2;
-        const centerYForTranslate = e.pageY - child.offsetTop - height / 2;
-        child.style.transform = `translate3d(${centerXForTranslate}px, ${centerYForTranslate}px, 0)`;
-        /* cach 2 */
-        // const centerYForLeft = e.pageX - width / 2;
-        // const centerXForTop = e.pageY - height / 2;
-        // child.style.left = `${centerYForLeft}px`;
-        // child.style.top = `${centerXForTop}px`;
-      }
-
-      function mouseover(e) {
-        // e.preventDefault();
-        const draggedEl = e.relatedTarget;
-        const stayEl = e.target;
-        const draggedElPos = draggedEl.getBoundingClientRect();
-        const stayElPos = stayEl.getBoundingClientRect();
-      }
-
-      function mouseleave() {
-        child.classList.remove("touched");
-        child.style.transform = `translate3d(${originLeft}px, ${originTop}px, 0)`;
-        document.removeEventListener("mousemove", mousemove);
-        document.removeEventListener("mouseup", mouseup);
-        document.removeEventListener("mouseenter", mouseover);
-        document.removeEventListener("mouseleave", mouseleave);
-      }
-
-      function mouseup(e) {
-        child.classList.remove("touched");
-        child.style.transform = `translate3d(${originLeft}px, ${originTop}px, 0)`;
-        document.removeEventListener("mousemove", mousemove);
-        document.removeEventListener("mouseup", mouseup);
-        document.removeEventListener("mouseover", mouseover);
-        document.removeEventListener("mouseleave", mouseleave);
-      }
+      el.addEventListener("dragend", function (e) {
+        el.classList.remove("dragging");
+        e.target.classList.remove("over");
+      });
     });
   }
 
-  run() {
-    this.addMouseEvent();
+  onContainerDragging(container) {
+    container.addEventListener("dragover", function (e) {
+      e.preventDefault();
+      const afterElement = getDragAfterElement(container, e.pageY);
+      const draggingEl = document.querySelector(".dragging");
+      if (afterElement) {
+        // afterElement.animate(
+        //   [
+        //     // keyframes
+        //     { transform: "translateY(0px)" },
+        //     { transform: `translateY(-172px)` },
+        //   ],
+        //   {
+        //     // timing options
+        //     duration: 300,
+        //   }
+        // );
+        container.insertBefore(draggingEl, afterElement);
+      } else {
+        container.appendChild(draggingEl);
+      }
+    });
+
+    function getDragAfterElement(container, y) {
+      const draggableElements = [...container.querySelectorAll(".box:not(.dragging)")];
+      return draggableElements.reduce(
+        (closet, curr) => {
+          const box = curr.getBoundingClientRect();
+          const offset = y - box.top - box.height / 2;
+          if (offset < 0 && offset > closet.offset) {
+            return { offset, element: curr };
+          } else {
+            return closet;
+          }
+        },
+        { offset: Number.NEGATIVE_INFINITY, element: null }
+      ).element;
+    }
+  }
+
+  bootstrap() {
+    const childrens = this.getChildrens();
+    const draggableEl = this.makeChildrenDraggable(childrens);
+    this.onChildrenDragging(draggableEl);
+
+    const container = this.getContainer();
+    this.onContainerDragging(container);
   }
 }
 
 const container = document.getElementsByClassName("container");
 const dws = new DragWithStyle(container);
-dws.run();
+dws.bootstrap();
