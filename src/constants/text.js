78ff554f715d509e12896401d2717f8fb276ea86
calledmeanh <calledmeanh@gmail.com>
2020-06-05 23:40:02 +0700

    create text constant

---
+++ src/constants/text.js
@@ -0,0 +1,10 @@
+export const OBJECT = "object";
+export const DRAG_EL = "dragEl";
+export const PLACEHOLDER_EL = "placeholderEl";
+export const DRAG_WITH_STYLE_DATA_ATTR = "[dragWithStyle]";
+export const TXTS = {
+  OBJECT,
+  DRAG_EL,
+  PLACEHOLDER_EL,
+  DRAG_WITH_STYLE_DATA_ATTR,
+};
