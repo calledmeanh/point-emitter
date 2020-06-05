e6be7e266e129afcd61f9b78f9b622afdb56f8a3
calledmeanh <calledmeanh@gmail.com>
2020-06-05 23:39:53 +0700

    create message constant

---
+++ src/constants/message.js
@@ -0,0 +1,8 @@
+// * means delete later
+export const ERROR = {
+  OBJECT: "INVALID OBJECT",
+  CONTAINER: "YOUR CONTAINER IS NULL",
+  STYLES: "YOUR STYLES SHOULD BE AN OBJECT", // *
+};
+
+export const MESSAGES = { ERROR };
