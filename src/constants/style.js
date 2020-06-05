880a54430091e39cdc230cfca288571a5e1b4a6c
calledmeanh <calledmeanh@gmail.com>
2020-06-05 23:39:58 +0700

    create style constant

---
+++ src/constants/style.js
@@ -0,0 +1,9 @@
+export const PLACEHOLDER_EL = {
+  border: "2px solid #a5dff8",
+  backgroundColor: "#a5dff8",
+  color: "#101928",
+  boxShadow: "0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12)",
+};
+
+
+export const STYLES = { PLACEHOLDER_EL };
