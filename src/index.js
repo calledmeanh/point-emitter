653cf534e17ad98a9ce5b544c90dc673cfaedac5
calledmeanh <calledmeanh@gmail.com>
2020-06-05 23:39:32 +0700

    [ FEATURE ] : default styles for placeholder el and drag el

--- src/index.js
+++ src/index.js
@@ -1,88 +1,104 @@
 import "./index.css";
+import { ERROR, STYLES, TXTS, EVENTS } from "./constants";
 
-class DragWithStyle {
-  constructor(container) {
-    this.container = container;
-  }
-
-  getContainer() {
-    return this.container[0];
-  }
-
-  getChildrens() {
-    const childrenByKeyValue = this.getContainer().children;
-    const childrensByIterable = Object.values(childrenByKeyValue);
-    const childrens = childrensByIterable.slice(0, childrensByIterable.length);
-    return childrens;
-  }
-
-  attachChildrenEvent(childrens) {
-    const container = this.getContainer();
-    childrens.forEach(function (child) {
-      child.addEventListener("mousedown", function (e) {
-        document.addEventListener("mousemove", mousemove);
-        document.addEventListener("mouseover", mouseover);
-        document.addEventListener("mouseup", mouseup);
-        /* clean these codes */
-        child.classList.add("touched");
-        const box = child.getBoundingClientRect();
-        child.style.position = "fixed";
-        child.style.top = box.top + "px";
-        child.style.left = box.left + "px";
-        child.style.width = box.width + "px";
-        child.style.height = box.height + "px";
-        child.style.zIndex = "5000";
-        child.style.pointEvents = "none";
-
-        const placeholderDiv = document.createElement("div");
-        placeholderDiv.style.width = child.style.width;
-        placeholderDiv.style.height = box.height + "px";
-        placeholderDiv.classList.add("placeholder");
-        placeholderDiv.textContent = child.textContent;
-        container.insertBefore(placeholderDiv, child);
-        /* clean these codes */
-
-        /* clean these codes */
-        function mousemove(e) {
-          child.hidden = false;
-          let x = e.pageX - box.left - box.width / 2;
-          let y = e.pageY - box.top - box.height / 2;
-          child.style.transform = `translate3d(${x}px,${y}px,0)`;
-          child.hidden = true;
+const DragWithStyle = function (styles = {}) {
+  const dragWithStyleEl = document.querySelector(TXTS.DRAG_WITH_STYLE_DATA_ATTR);
+  if (!dragWithStyleEl) throw new Error(ERROR.CONTAINER);
+  return {
+    /**
+     * return true if this is an object
+     * @param {*} obj obj given by the user
+     */
+    _isObject: function (obj) {
+      if (typeof obj !== TXTS.OBJECT) return false;
+      return true;
+    },
+    /**
+     * return true if this object is empty
+     * @param {*} obj obj give by the user
+     */
+    _isEmptyObject: function (obj) {
+      if (!this._isObject(obj)) throw new Error(ERROR.OBJECT);
+      for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+          return false;
         }
-        /* clean these codes */
+      }
+      return JSON.stringify(obj) === JSON.stringify({});
+    },
+    /**
+     * return true if it has default styles
+     * @param {*} styles user's style
+     * ```
+     *  const styles = {
+     *    const placeholderEl = {
+     *    border: "2px solid #a5dff8",
+     *    backgroundColor: "#a5dff8",
+     *    color: "#101928",
+     *    boxShadow: "0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12)",
+     *    };
+     *    const dragEl = {
+     *    border: "2px solid #a5dff8",
+     *    backgroundColor: "#a5dff8",
+     *    color: "#101928",
+     *    boxShadow: "0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12)",
+     *    };
+     *  }
+     * ```
+     */
+    _isHasDefaultStyles: function (styles) {
+      if (styles.placeholderEl !== undefined && styles.dragEl !== undefined) return true;
+      else return false;
+    },
+    _createDefaultStyles: function (styles) {
+      /*  
+          create default style if given style is empty or (styles = {})
+          style.placeholderEl, style.dragEl are both empty (styles = {placeholderEl: {}, dragEl: {}})
+      */
+      let defaultPlaceholderEl = null;
+      let defaultDragEl = null;
+      if (this._isEmptyObject(styles) && !this._isHasDefaultStyles(styles)) {
+        defaultPlaceholderEl = STYLES.PLACEHOLDER_EL;
+        return { defaultPlaceholderEl, defaultDragEl };
+      }
 
-        function mouseover(e) {
-          e.stopPropagation();
-          if (e.relatedTarget) {
-            console.log("target: ", e.target);
-            console.log("related: ", e.relatedTarget);
+      const isFoundDefaultStyle = [false, false];
+      for (const style in styles) {
+        if (styles.hasOwnProperty(style)) {
+          if (style === TXTS.DRAG_EL) {
+            defaultDragEl = styles[style];
+            isFoundDefaultStyle[0] = true;
           }
+          if (style === TXTS.PLACEHOLDER_EL) {
+            defaultPlaceholderEl = styles[style];
+            isFoundDefaultStyle[1] = true;
+          }
+          // found 2 default styles then stop loop
+          if (isFoundDefaultStyle[0] && isFoundDefaultStyle[1]) break;
         }
+        return { defaultPlaceholderEl, defaultDragEl };
+      }
+    },
+    _initStyle: function () {
+      const { defaultPlaceholderEl, defaultDragEl } = this._createDefaultStyles(styles);
+      return { defaultPlaceholderEl, defaultDragEl };
+    },
+    apply: function () {
+      const { defaultPlaceholderEl, defaultDragEl } = this._initStyle();
 
-        function mouseup(e) {
-          /* clean these codes */
-          child.style = "";
-          child.classList.remove("touched");
-          container.removeChild(placeholderDiv);
-          /* clean these codes */
-          document.removeEventListener("mousemove", mousemove);
-          document.removeEventListener("mouseover", mouseover);
-          document.removeEventListener("mouseup", mouseup);
-        }
+      const childrenByKeyValue = dragWithStyleEl.children;
+      const childrenByIterable = Object.values(childrenByKeyValue);
+      const childrenLength = childrenByIterable.length;
+
+      if (!childrenLength) return;
+      childrenByIterable.forEach(function (child) {
+        child.addEventListener(EVENTS.MOUSEDOWN, function (e) {
+          console.table(defaultPlaceholderEl);
+          console.table(defaultDragEl);
+        });
       });
-      child.addEventListener("dragstart", function (e) {
-        return false;
-      });
-    });
-  }
+    },
+  };
+};
 
-  bootstrap() {
-    const childrens = this.getChildrens();
-    this.attachChildrenEvent(childrens);
-  }
-}
-
-const container = document.getElementsByClassName("container");
-const dws = new DragWithStyle(container);
-dws.bootstrap();
+new DragWithStyle().apply();
