cabf8a24e10900059903f20177f5f0a3f4897790
calledmeanh <calledmeanh@gmail.com>
2020-06-07 15:14:34 +0700

    update txt constant file

--- src/constants/text.js
+++ src/constants/text.js
@@ -1,10 +1,17 @@
 export const OBJECT = "object";
 export const DRAG_EL = "dragEl";
 export const PLACEHOLDER_EL = "placeholderEl";
+export const DRAG_WITH_STYLE = "dragWithStyle";
 export const DRAG_WITH_STYLE_DATA_ATTR = "[dragWithStyle]";
+export const DRAG_WITH_STYLE_ITEM = "dragWithStyleItem";
+export const DRAG_WITH_STYLE_ITEM_DATA_ATTR = "[dragWithStyleItem]";
+export const DRAG_WTIH_STYLE_PLACEHOLDER_EL = "dragWithStylePlaceholder";
 export const TXTS = {
   OBJECT,
   DRAG_EL,
   PLACEHOLDER_EL,
   DRAG_WITH_STYLE_DATA_ATTR,
+  DRAG_WITH_STYLE_ITEM,
+  DRAG_WITH_STYLE_ITEM_DATA_ATTR,
+  DRAG_WTIH_STYLE_PLACEHOLDER_EL,
 };
