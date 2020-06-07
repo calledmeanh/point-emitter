840fc00342daac63d4451de166dd769c3e5f7197
calledmeanh <calledmeanh@gmail.com>
2020-06-07 15:14:23 +0700

    update style

--- src/constants/style.js
+++ src/constants/style.js
@@ -1,9 +1,14 @@
+export const DEFAULT = {
+  backgroundColor: "",
+  color: "",
+  opacity: "",
+  boxShadow: "",
+};
 export const PLACEHOLDER_EL = {
-  border: "2px solid #a5dff8",
-  backgroundColor: "#a5dff8",
+  backgroundColor: "#fff",
   color: "#101928",
+  opacity: "0.5",
   boxShadow: "0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12)",
 };
 
-
-export const STYLES = { PLACEHOLDER_EL };
+export const STYLES = { PLACEHOLDER_EL, DEFAULT };
