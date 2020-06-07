8034b19db542d4fb56612ce090c82bb91519e976
calledmeanh <calledmeanh@gmail.com>
2020-06-07 15:14:42 +0700

    update event constant file

--- src/constants/event.js
+++ src/constants/event.js
@@ -1,5 +1,6 @@
 export const MOUSEDOWN = "mousedown";
 export const MOUSEUP = "mouseup";
 export const MOUSEMOVE = "mousemove";
+export const DRAGSTART = "dragstart";
 
-export const EVENTS = { MOUSEDOWN, MOUSEUP, MOUSEMOVE };
+export const EVENTS = { MOUSEDOWN, MOUSEUP, MOUSEMOVE, DRAGSTART };
