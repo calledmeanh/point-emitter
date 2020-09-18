071437629635dd1aa6e9777812317834b8548a21
calledmeanh <calledmeanh@gmail.com>
2020-09-19 01:10:51 +0700

    config weback for typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = {
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
     ],
   },
-  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   devServer: {
-    open: true,
     contentBase: "./dist",
+    open: true,
     port: 3000,
   },
 };
